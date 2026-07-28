c97ef6dd24cec59c4bd6ed1060458c116127cb75
fildosobral-sys <fildosobral@gmail.com>
2026-07-28 15:00:44 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fs-central-v42-downloads-repetidos";
+const CACHE_NAME = "fs-central-v43-cotacoes-download-fechar";
 const APP_FILES = [
   "/",
   "/index.html",
@@ -12,7 +12,7 @@ const APP_FILES = [
   "/favicon.svg",
   "/icon-192.png",
   "/icon-512.png",
-  "/pwa-update.js?v=42",
+  "/pwa-update.js?v=43",
   "/zenir.png",
   "/draft-protection.js?v=29",
   "/home-transition.js?v=29"
