889806e6eed57a01ba0c4ac2e99f9e4ebce1159c
fildosobral-sys <fildosobral@gmail.com>
2026-07-28 14:04:46 -0300

    Add files via upload

--- pwa-update.js
+++ pwa-update.js
@@ -1,7 +1,7 @@
 (function(){
   if(!('serviceWorker' in navigator) || !/^https?:$/.test(location.protocol)) return;
 
-  var VERSION = '38';
+  var VERSION = '39';
   var path = location.pathname.replace(/\/+$/, '/');
   var isCentral = path === '/' || /\/index\.html$/i.test(location.pathname);
   if(!isCentral) return;
