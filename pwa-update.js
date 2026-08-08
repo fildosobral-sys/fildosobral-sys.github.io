3d26d92b13f0335240faba2665f2a0de8850d363
fildosobral-sys <fildosobral@gmail.com>
2026-08-08 10:30:09 -0300

    Add files via upload

--- pwa-update.js
+++ pwa-update.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
 
-  var VERSION = '33';
+  var VERSION = '34';
 
   function isQuotaError(error){
     return !!error && (
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fs-central-v33-estavel";
+const CACHE_NAME = "fs-central-v34-estavel";
 
 const APP_FILES = [
   "/",
